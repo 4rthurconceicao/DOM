f62a4a73f06e173eb2978d148d9b98ef1e1ca202
4rthurconceicao <arthurconceicaosouza1@gmail.com>
2025-04-09 14:23:15 -0300

    adicionei aquirvos

---
+++ script.js
@@ -0,0 +1,23 @@
+// Função que volta o fundo do body para a cor original (cinza escuro)
+function Volta(){
+    document.body.style.background = 'rgb(32, 31, 33)'
+   }
+   // Função que muda o fundo do body para VERMELHO quando o mouse entra na div #Blue
+     function BLUE(){
+ 
+       document.body.style.background = 'Blue'
+     }
+    // Função que muda o fundo do body para ROXO quando o mouse entra na div #Purple
+     function PURPLE() {
+      
+        document.body.style.background = 'purple'
+     }
+     // Função que muda o fundo do body para AMARELO quando o mouse entra na div #yellow
+     function YELLOW(){
+         document.body.style.background = 'yellow'
+     }
+      // Função que muda o fundo do body para VERMELHO quando o mouse entra na div #Red
+     function RED(){
+      document.body.style.background = 'Red'
+     }
+
